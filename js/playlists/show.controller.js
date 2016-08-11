2eefba6510b193ce3a093ef36a810ffa56eeb1ab
Julia Najafi <Julia.Najafi@gmail.com>
2016-08-11 11:11:22 -0400

    trying to save searched word to database

--- js/playlists/show.controller.js
+++ js/playlists/show.controller.js
@@ -7,41 +7,48 @@
     "PlaylistFactory",
     "$stateParams",
     "WordFactory",
-    "VocabFactory",
+    "$scope",
     PlaylistShowControllerFunction
   ]);
 
-  function PlaylistShowControllerFunction(PlaylistFactory, $stateParams, VocabFactory, WordFactory){
-    this.playlist = PlaylistFactory.get({id: $stateParams.id});
-    this.vocab = VocabFactory.get({id: $stateParams.id}).$promise.then(function(response){
-      console.log(response)
-    })
-    
-
-    console.log("in show controller")
-
+  function PlaylistShowControllerFunction(PlaylistFactory, $stateParams, WordFactory, $scope){
     var vm = this;
-    var searchedWords = [];
 
+    //calling my api for playlist
+    vm.playlist = PlaylistFactory.get({id: $stateParams.id})
+
+
+
+    // searching for a word in api from wordFactory
+    var searchedWords = [];
     vm.search = function(){
       new WordFactory.get({phrase: vm.word}).$promise.then(function(response){
-        console.log(response.tuc[0].phrase.text)
         searchedWords.push(response.tuc[0].phrase.text)
-        console.log(searchedWords)
         vm.usersearch = response.tuc[0].phrase.text
         return response.tuc[0].phrase.text
-      }).then(function(response){
-        console.log(response)
       })
+    }
+
+    vm.save = function(){
+      console.log(vm.usersearch)
+      vm.playlist.words.push({
+        englishword: vm.word,
+        otherword: vm.usersearch
+      })
+      vm.playlist.$save({id: $stateParams.id})
+      console.log(vm.playlist.words)
     }
 
 
 
 
-
+    // what i need to do is put an add button then update current playlist with the added word
     // this.update = function () {
     //   this.bleet.$update({id: $stateParams.id})
     // };
+
+
+
     // this.destroy = function(){
     //   this.bleet.$delete({id: $stateParams.id});
     // }
